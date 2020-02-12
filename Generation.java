cbc7e603e1049aacd8b1dc1a49bcee5803ae3afa
Logan <60444707+ljborrero@users.noreply.github.com>
2020-02-12 14:18:53 -0500

    Update Generation.java

--- Generation.java
+++ Generation.java
@@ -8,15 +8,21 @@ public class Generation {
     Fly bestFly;
     int genNum;
     int stepNum;
-    int popSize;
+    int popSize = 0;
     Generation(int popS){
-        
+        population = new Fly[popS];
+        bestFly = new Fly(stepNum);
+        popSize = 0;
     }
     public ImageView[] show(){
         return null;
     }
     public void calcBestFly(){
-        
+        for(int i=0; i<popSize; i++){
+         if(population.length!=0)
+             bestFly.fitness = 1;
+         else
+             bestFly.fitness = -1;
     }
     public boolean allDead(){
         return false;
