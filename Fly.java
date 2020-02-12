262ca50f7a4dddca35f712afb75fbdada315c8e8
jmccloud827 <60444521+jmccloud827@users.noreply.github.com>
2020-02-12 14:21:44 -0500

    Update Fly.java

--- Fly.java
+++ Fly.java
@@ -14,24 +14,73 @@ public class Fly {
     double fitness;
     int xPos;
     int yPos;
-    final int mutationRate = 15;
+    int mutationRate;
     Random rnd = new Random();
-    Fly(int sNum){
-        
+    Fly(int sNum, int mRate){
+        stepNum = sNum;
+        mutationRate = mRate;
+        moves = new int[stepNum][2];
+        body = new ImageView(new Image(getClass().getResourceAsStream("fly.png")));
+        body.setFitHeight(10);
+        body.setFitWidth(10);
+        dead = false;
+        goal = false;
+        moveNum = 0;
+        fitness = 0;
+        xPos = 195;
+        yPos = 695;
     }
     public void randomize(){
-        
+        for(int i = 0; i < stepNum; i++){
+            moves[i][0] = (rnd.nextInt(9)-4);
+            moves[i][1] = (rnd.nextInt(9)-4);
+        }
     }
     public ImageView move(){
-        return null;
+        isDead();
+        if(!dead){
+            int rotate = 0;
+            if(moves[moveNum][0] == 0 && moves[moveNum][1] > 0){
+                rotate = 90;
+            } else if(moves[moveNum][0] == 0 && moves[moveNum][1] < 0){
+                rotate = 270;
+            } else if(moves[moveNum][0] > 0){
+                rotate = (int)(Math.atan(moves[moveNum][1]/moves[moveNum][0])*180/Math.PI);
+            } else if(moves[moveNum][0] < 0){
+                rotate = (int)(Math.atan(moves[moveNum][1]/moves[moveNum][0])*180/Math.PI)+180;
+            }
+            xPos += moves[moveNum][0];
+            yPos += moves[moveNum][1];
+            moveNum++;
+            body.setX(xPos);
+            body.setY(yPos);
+            body.setRotate(rotate);
+        }
+        return body;
     }
     public void isDead(){
-        
+        double dist = Math.sqrt(Math.pow(xPos - 195, 2) + Math.pow(yPos - 60, 2));
+        if(moveNum >= stepNum || xPos < 0 || yPos < 0 || xPos > 390 || yPos > 790){
+            dead = true;
+        } else if(dist <= 20){
+            dead = true;
+            goal = true;
+        }
     }
     public void mutate(){
-        
+        for(int i = 0; i < stepNum; i++){
+            if((rnd.nextInt(100) + 1) < mutationRate){
+                moves[i][0] = (rnd.nextInt(9)-4);
+                moves[i][1] = (rnd.nextInt(9)-4);
+            }
+        }
     }
     public void reset(){
-        
+        dead = false;
+        goal = false;
+        moveNum = 0;
+        fitness = 0;
+        xPos = 195;
+        yPos = 695;
     }
 }
